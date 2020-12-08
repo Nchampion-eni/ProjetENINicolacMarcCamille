ae37d01ad9416a696dc8f34844c673419903cc31
NChampion <nicolaschampion.bafa@gmail.com>
2020-12-08 15:24:44 +0100

    ergdrd

--- ProjetEnchereENI/src/fr/eni/ecole/enchere/bo/Utilisateur.java
+++ ProjetEnchereENI/src/fr/eni/ecole/enchere/bo/Utilisateur.java
@@ -1,7 +1,7 @@
 package fr.eni.ecole.enchere.bo;
 
 public class Utilisateur {
-	
+
 	private int id;
 	private String nom;
 	private String prenom;
@@ -13,6 +13,7 @@ public class Utilisateur {
 	private String motDePasse;
 	private String credit;
 	private String administrateur;
+
 	public Utilisateur(int id, String nom, String prenom, String email, String telephone, String rue, String codePostal,
 			String ville, String motDePasse, String credit, String administrateur) {
 		super();
@@ -28,78 +29,100 @@ public class Utilisateur {
 		this.credit = credit;
 		this.administrateur = administrateur;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public String getPrenom() {
 		return prenom;
 	}
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+
 	public String getRue() {
 		return rue;
 	}
+
 	public void setRue(String rue) {
 		this.rue = rue;
 	}
+
 	public String getCodePostal() {
 		return codePostal;
 	}
+
 	public void setCodePostal(String codePostal) {
 		this.codePostal = codePostal;
 	}
+
 	public String getVille() {
 		return ville;
 	}
+
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
+
 	public String getMotDePasse() {
 		return motDePasse;
 	}
+
 	public void setMotDePasse(String motDePasse) {
 		this.motDePasse = motDePasse;
 	}
+
 	public String getCredit() {
 		return credit;
 	}
+
 	public void setCredit(String credit) {
 		this.credit = credit;
 	}
+
 	public String getAdministrateur() {
 		return administrateur;
 	}
+
 	public void setAdministrateur(String administrateur) {
 		this.administrateur = administrateur;
 	}
+
 	@Override
 	public String toString() {
 		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", telephone="
 				+ telephone + ", rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + ", motDePasse="
 				+ motDePasse + ", credit=" + credit + ", administrateur=" + administrateur + "]";
 	}
-	
 
 }
