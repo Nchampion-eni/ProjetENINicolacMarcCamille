a5b16fd85140c44fe0a5cebdde9128b070e345bc
marcfossati <marcfossati@aria2.home>
2020-12-08 11:33:43 +0100

    MF: utilisateur

---
+++ ProjetEnchereENI/src/fr/eni/bo/Utilisateur.java
@@ -0,0 +1,20 @@
+package fr.eni.bo;
+
+public class Utilisateur {
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	private String credit;
+	private String administrateur;
+	
+	
+	
+
+}
