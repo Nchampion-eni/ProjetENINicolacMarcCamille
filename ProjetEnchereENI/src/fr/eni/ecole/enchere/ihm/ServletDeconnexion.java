b1307afeb5a4b4071f759260ccf1407be5269974
Camille <Camille@Camille-PC.home>
2020-12-09 10:38:54 +0100

    CP : ServletDeconnexion + connexion.jsp

---
+++ ProjetEnchereENI/src/fr/eni/ecole/enchere/ihm/ServletDeconnexion.java
@@ -0,0 +1,40 @@
+package fr.eni.ecole.enchere.ihm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletDeconnexion
+ */
+@WebServlet("/ServletDeconnexion")
+public class ServletDeconnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Destruction de la session en cours
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		//Redirection vers la page d'accueil
+		response.sendRedirect("index.html");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
